5d4bd3d19ebe7e46e1b8822f596ca0c1ece81c7f
jameso-student <s3782490@student.rmit.edu.au>
2023-08-17 13:40:58 +1000

    Fixed parameters

--- lib/credly/connection.rb
+++ lib/credly/connection.rb
@@ -5,31 +5,31 @@ module Credly
     def initialize(url, access_token)
       @connection = Faraday.new(url: url) do |builder|
         builder.request :json
-        builder.headers['Accept'] = 'application/json'
+        builder.headers[:accept] = 'application/json'
         builder.response :json
         builder.request :authorization, 'Basic', -> { Base64.strict_encode64(access_token) }
       end
     end
 
-    def delete(path, **params)
+    def delete(path, params = {})
       request(:delete, path, params)
     end
 
-    def get(path, **params)
+    def get(path, params = {})
       request(:get, path, params)
     end
 
-    def post(path, **params)
+    def post(path, params = {})
       request(:post, path, params)
     end
 
-    def put(path, **params)
+    def put(path, params = {})
       request(:put, path, params)
     end
 
     def request(method, path, params)
       puts @connection.url_prefix
-      @connection.public_send(method, path, params)
+      @connection.send(method, path, params)
     end
   end
 end
