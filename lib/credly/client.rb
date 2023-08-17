99768b4567562cd5c0e2ba9a90eaf5671c490bbf
jameso-student <s3782490@student.rmit.edu.au>
2023-08-17 14:54:19 +1000

    Modified client initialization params.

--- lib/credly/client.rb
+++ lib/credly/client.rb
@@ -1,19 +1,21 @@
 module Credly
   class Client
     include Credly::Actions::Badges
-    attr_reader :organization_id, :access_token
-    def initialize(base_url:, version: 'v1', organization_id:, access_token:)
-      @url = [base_url, version].join('/')
+    attr_reader :organization_id
+
+    BASE_URL_P  = "https://api.credly.com"
+    BASE_URL_S  = "https://sandbox-api.credly.com"
+
+    def initialize(version: 'v1', organization_id:, access_token:, sandbox: false)
+      @base_url = sandbox  ? BASE_URL_S : BASE_URL_P
+      @url = [@base_url, version].join('/')
       @version = version
       @organization_id = organization_id
       @access_token = access_token
     end
 
     def connection
-      unless defined? @connection
-        @connection = Connection.new(@url, @access_token)
-      end
-      @connection
+      @connection ||= Connection.new(@url, @access_token)
     end
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe "Authorization" do
   it "creates a client instance with a fake access token" do
     client = Credly::Client.new(
-      base_url: "https://sandbox-api.credly.com",
       organization_id: "4ceb7c6c-71c1-4836-aaba-0c15323f7e1d",
-      access_token: "Fake Token"
+      access_token: "Fake Token",
+      sandbox: true
     )
     expect(client).to be_an_instance_of(Credly::Client)
   end
 
   it "creates a client instance with a real access token" do
     client = Credly::Client.new(
-      base_url: "https://sandbox-api.credly.com",
       organization_id: "4ceb7c6c-71c1-4836-aaba-0c15323f7e1d",
-      access_token: ENV["AUTH_TOKEN"].to_s
+      access_token: ENV["AUTH_TOKEN"].to_s,
+      sandbox: true
     )
     res = client.badges_get
     expect(res.status).to equal(200)
