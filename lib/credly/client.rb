30c6f5dcd37ff7f04a0b582b34b35357a724d795
jameso-student <s3782490@student.rmit.edu.au>
2023-08-21 17:48:06 +1000

    Added more getters and setters

--- lib/credly/client.rb
+++ lib/credly/client.rb
@@ -1,21 +1,27 @@
 module Credly
   class Client
     include Credly::Actions::Badges
-    attr_reader :organization_id
+    attr_reader :organization_id, :url
+    attr_accessor :sandbox
 
     BASE_URL_P  = "https://api.credly.com"
     BASE_URL_S  = "https://sandbox-api.credly.com"
 
     def initialize(version: 'v1', organization_id:, access_token:, sandbox: false)
-      @base_url = sandbox  ? BASE_URL_S : BASE_URL_P
+      @base_url = sandbox ? BASE_URL_S : BASE_URL_P
       @url = [@base_url, version].join('/')
       @version = version
       @organization_id = organization_id
       @access_token = access_token
+      @sandbox = sandbox
     end
 
     def connection
       @connection ||= Connection.new(@url, @access_token)
+    end
+
+    def base_url
+      @sandbox ? BASE_URL_S : BASE_URL_P
     end
   end
 end
