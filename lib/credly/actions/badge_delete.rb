34193d2bd791dc5eedc9c161baccd284acf836e9
Shuning-Xu <s3827146@student.rmit.edu.au>
2023-08-29 16:44:38 +1000

    Feature/delete badge

--- lib/credly/actions/badge_delete.rb
+++
@@ -1,9 +0,0 @@
-module Credly
-    module Actions
-      module BadgeDelete
-        def badges_delete(params = {})
-          connection.get("organizations/#{organization_id}/badges", params)
-        end
-      end
-    end
-  end
