ed41d9fd216ec8899618737b8fd9be9343453dfb
jameso-student <s3782490@student.rmit.edu.au>
2023-09-12 22:29:50 +1000

    Fixed errors

--- spec/credly-ruby/actions/badge_templates_spec.rb
+++ spec/credly-ruby/actions/badge_templates_spec.rb
@@ -14,34 +14,38 @@ RSpec.describe Credly::Actions::BadgeTemplates do
       stub = stub_request(:get, "#{@client.url}/organizations/#{@client.organization_id}/badge_templates")
 
       @client.badge_templates_list
-  describe '#badge_templates_get' do
-    it 'issues the correct request without query params' do
-      stub = stub_request(:get, "#{@client.url}/organizations/#{@client.organization_id}/badge_templates")
-
-      @client.badge_templates_get
 
       expect(stub).to have_been_requested
     end
-  end
+    describe '#badge_templates_get' do
+      it 'issues the correct request without query params' do
+        stub = stub_request(:get, "#{@client.url}/organizations/#{@client.organization_id}/badge_templates")
 
-  describe '#badge_templates_list' do
-    it 'issues the correct request with query params' do
-      stub = stub_request(:get, "#{@client.url}/organizations/#{@client.organization_id}/badge_templates")
-             .with(query: { filter: 'query::FakeParam' })
-      
-      @client.badge_templates_list({ filter: 'query::FakeParam' })
+        @client.badge_templates_list
 
-      expect(stub).to have_been_requested
+        expect(stub).to have_been_requested
+      end
     end
-  end
 
-  describe '#badge_template_get' do
-    it 'issues the correct request with ID' do
-      stub = stub_request(:get, "#{@client.url}/organizations/#{@client.organization_id}/badge_templates/1")
+    describe '#badge_templates_list' do
+      it 'issues the correct request with query params' do
+        stub = stub_request(:get, "#{@client.url}/organizations/#{@client.organization_id}/badge_templates")
+                 .with(query: { filter: 'query::FakeParam' })
 
-      @client.badge_template_get(1)
+        @client.badge_templates_list({ filter: 'query::FakeParam' })
 
-      expect(stub).to have_been_requested
+        expect(stub).to have_been_requested
+      end
+    end
+
+    describe '#badge_template_get' do
+      it 'issues the correct request with ID' do
+        stub = stub_request(:get, "#{@client.url}/organizations/#{@client.organization_id}/badge_templates/1")
+
+        @client.badge_template_get(1)
+
+        expect(stub).to have_been_requested
+      end
     end
   end
 end
