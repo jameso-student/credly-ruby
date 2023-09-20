854f76209028f1992073836499b26e88ef6e52ac
jameso-student <s3782490@student.rmit.edu.au>
2023-09-20 11:22:23 +1000

    Organization ID now must be passed to methods that need it

--- lib/credly/actions/badges.rb
+++ lib/credly/actions/badges.rb
@@ -3,27 +3,27 @@
 module Credly
   module Actions
     module Badges
-      def badge_list(params = {})
-        connection.get("organizations/#{organization_id}/badges", params)
+      def badge_list(organization_id, badge_list_params = {})
+        connection.get("organizations/#{organization_id}/badges", badge_list_params)
       end
 
-      def badge_issue(params)
-        connection.post("organizations/#{organization_id}/badges", params)
+      def badge_issue(organization_id, badge_issue_params)
+        connection.post("organizations/#{organization_id}/badges", badge_issue_params)
       end
 
-      def badge_delete(badge_id)
+      def badge_delete(organization_id, badge_id)
         connection.delete("organizations/#{organization_id}/badges/#{badge_id}")
       end
 
-      def badge_list_bulk(params = {})
-        connection.get("organizations/#{organization_id}/high_volume_issued_badge_search", params)
+      def badge_list_bulk(organization_id, badge_list_bulk_params = {})
+        connection.get("organizations/#{organization_id}/high_volume_issued_badge_search", badge_list_bulk_params)
       end
 
-      def badge_revoke(badge_id, revoke_params)
+      def badge_revoke(organization_id, badge_id, revoke_params)
         connection.put("organizations/#{organization_id}/badges/#{badge_id}/revoke", revoke_params)
       end
 
-      def badge_replace(badge_id, replace_params)
+      def badge_replace(organization_id, badge_id, replace_params)
         connection.post("organizations/#{organization_id}/badges/#{badge_id}/replace", replace_params)
       end
     end
