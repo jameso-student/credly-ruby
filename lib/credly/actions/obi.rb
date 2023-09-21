6abbb798a8966657069c7108f2f939c032330029
James Ogilvie <89754212+jameso-student@users.noreply.github.com>
2023-09-21 20:34:25 +1000

    Get badge issuer (#19)
    * Implemented OBI issuer endpoint
    
    * Implemented OBI issuer endpoint test

--- lib/credly/actions/obi.rb
+++ lib/credly/actions/obi.rb
@@ -3,12 +3,16 @@
 module Credly
   module Actions
     module OBI
-      def badge_assertion_get(badge_id)
+      def obi_badge_assertion_get(badge_id)
         connection.get("obi/v2/badge_assertions/#{badge_id}")
       end
 
-      def badge_class_get(badge_class_id)
+      def obi_badge_class_get(badge_class_id)
         connection.get("obi/v2/badge_classes/#{badge_class_id}")
+      end
+
+      def obi_issuer_get(issuer_id)
+        connection.get("obi/v2/issuers/#{issuer_id}")
       end
     end
   end
