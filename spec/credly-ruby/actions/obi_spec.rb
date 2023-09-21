bccf0caa62f2ee771e21271384802e5fd8379931
jameso-student <s3782490@student.rmit.edu.au>
2023-09-21 17:03:23 +1000

    Implemented OBI issuer endpoint test

--- spec/credly-ruby/actions/obi_spec.rb
+++ spec/credly-ruby/actions/obi_spec.rb
@@ -9,23 +9,34 @@ RSpec.describe Credly::Actions::OBI do
     @client = Credly::Client.new(auth_token: 'token')
   end
 
-  describe '#badge_assertion_get' do
+  describe '#obi_badge_assertion_get' do
     it 'issues the correct GET request' do
       badge_id = 1
       stub = stub_request(:get, "#{@client.url}/obi/v2/badge_assertions/#{badge_id}")
 
-      @client.badge_assertion_get(badge_id)
+      @client.obi_badge_assertion_get(badge_id)
 
       expect(stub).to have_been_requested
     end
   end
 
-  describe '#badge_class_get' do
+  describe '#obi_badge_class_get' do
     it 'issues the correct GET request' do
       badge_class_id = 1
       stub = stub_request(:get, "#{@client.url}/obi/v2/badge_classes/#{badge_class_id}")
 
-      @client.badge_class_get(badge_class_id)
+      @client.obi_badge_class_get(badge_class_id)
+
+      expect(stub).to have_been_requested
+    end
+  end
+
+  describe '#obi_issuer_get' do
+    it 'issues the correct GET request' do
+      issuer_id = 1
+      stub = stub_request(:get, "#{@client.url}/obi/v2/issuers/#{issuer_id}")
+
+      @client.obi_issuer_get(issuer_id)
 
       expect(stub).to have_been_requested
     end
