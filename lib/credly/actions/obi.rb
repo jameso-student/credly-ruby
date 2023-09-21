3296a7d26152f5d4d62af9f30544c51c54400b9b
jameso-student <s3782490@student.rmit.edu.au>
2023-09-21 15:07:44 +1000

    Implemented badge class retrieval

--- lib/credly/actions/obi.rb
+++ lib/credly/actions/obi.rb
@@ -6,6 +6,10 @@ module Credly
       def badge_assertion_get(badge_id)
         connection.get("obi/v2/badge_assertions/#{badge_id}")
       end
+
+      def badge_class_get(badge_class_id)
+        connection.get("obi/v2/badge_classes/#{badge_class_id}")
+      end
     end
   end
 end
