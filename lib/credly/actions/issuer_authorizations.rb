cf4f41f62cf4ca3f1ad11fffd7490a805db63c3c
Shuning-Xu <s3827146@student.rmit.edu.au>
2023-09-27 16:35:42 +1000

    rename method name

--- lib/credly/actions/issuer_authorizations.rb
+++ lib/credly/actions/issuer_authorizations.rb
@@ -2,10 +2,10 @@ module Credly
     module Actions
       module IssuerAuthorizations
 
-        def issuers_get(organization_id)
+        def issuer_get(organization_id)
           connection.get("organizations/#{organization_id}/issuers")
         end
-        
+
       end
     end
   end
