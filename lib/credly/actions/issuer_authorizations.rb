ed7e1bcdd4dd228899a20a4ac689c482be46feae
Shuning-Xu <s3827146@student.rmit.edu.au>
2023-09-27 16:43:14 +1000

    update IssuerAuthorizations method

--- lib/credly/actions/issuer_authorizations.rb
+++ lib/credly/actions/issuer_authorizations.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 module Credly
-    module Actions
-      module IssuerAuthorizations
-
-        def issuer_get(organization_id)
-          connection.get("organizations/#{organization_id}/issuers")
-        end
-
+  module Actions
+    module IssuerAuthorizations
+      def issuer_get(organization_id)
+        connection.get("organizations/#{organization_id}/issuers")
       end
     end
   end
+end
