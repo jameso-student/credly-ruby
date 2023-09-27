7c9bf2fa1583838fc7819f770ca214ab9be1fdfa
Shuning-Xu <s3827146@student.rmit.edu.au>
2023-09-27 16:34:56 +1000

    Implemented issuer get method test

---
+++ spec/credly-ruby/actions/issuer_authorizations_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Credly::Actions::IssuerAuthorizations do
+  subject { @client }
+
+  before do
+    @client = Credly::Client.new(auth_token: 'token')
+    @organization_id = 1
+  end
+
+  describe '#issuer_get' do
+    it 'issues the correct GET request' do
+      stub = stub_request(:get, "#{@client.url}/organizations/#{@organization_id}/issuers")
+
+      @client.issuer_get(@organization_id)
+
+      expect(stub).to have_been_requested
+    end
+  end
+
+end
